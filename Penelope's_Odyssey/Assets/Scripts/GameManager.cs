b5720658564b4fda1dacaa490feee2adfe35b4b2
DRaven_McCoy <dravenmccoy1@gmail.com>
2021-09-12 16:23:29 -0500

    Added a prototype hunger bar that decreases as the game runs.

--- Penelope's_Odyssey/Assets/Scripts/GameManager.cs
+++ Penelope's_Odyssey/Assets/Scripts/GameManager.cs
@@ -2,9 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public float hunger = 100;
+    public float sub = 2;
+    public Slider HungerBar;
+
+    public float getHunger()
+    {
+        hunger = hunger - sub/1800;
+        return hunger;
+    }
+
+    public void UpdateScreen()
+    {
+        HungerBar.value = hunger;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +30,8 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        getHunger();
+        UpdateScreen();
     }
 
     public void CloseGame()
