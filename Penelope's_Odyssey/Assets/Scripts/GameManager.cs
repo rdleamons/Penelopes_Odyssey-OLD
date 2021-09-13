9720df4045b7c140d3db9e0c32695a98c0e29e56
Rebekah <rdleamons@gmail.com>
2021-09-12 20:01:50 -0500

    Improved lose condition

--- Penelope's_Odyssey/Assets/Scripts/GameManager.cs
+++ Penelope's_Odyssey/Assets/Scripts/GameManager.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public float hunger = 100;
     public float sub = 2;
     public Slider HungerBar;
-    
+    public TextMeshProUGUI loseText;
+
+    private void Start()
+    {
+        loseText.enabled = false;
+    }
 
     void Update()
     {
@@ -18,7 +24,7 @@ public class GameManager : MonoBehaviour
 
         if(hunger <= 0)
         {
-            SceneManager.LoadScene("StartScreen");
+            StartCoroutine("lose");
         }
     }
 
@@ -31,5 +37,12 @@ public class GameManager : MonoBehaviour
     public void UpdateScreen()
     {
         HungerBar.value = hunger;
+    }
+
+    IEnumerator lose()
+    {
+        loseText.enabled = true;
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("StartScreen");
     }
 }
