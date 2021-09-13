7ad09414d221b2a94c0d1d9a8e73306b81da25bd
Rebekah <rdleamons@gmail.com>
2021-09-12 19:40:08 -0500

    Adding exit condition

--- Penelope's_Odyssey/Assets/Scripts/GameManager.cs
+++ Penelope's_Odyssey/Assets/Scripts/GameManager.cs
@@ -9,38 +9,27 @@ public class GameManager : MonoBehaviour
     public float hunger = 100;
     public float sub = 2;
     public Slider HungerBar;
+    
+
+    void Update()
+    {
+        getHunger();
+        UpdateScreen();
+
+        if(hunger <= 0)
+        {
+            SceneManager.LoadScene("StartScreen");
+        }
+    }
 
     public float getHunger()
     {
-        hunger = hunger - sub/1800;
+        hunger = hunger - sub / 1800;
         return hunger;
     }
 
     public void UpdateScreen()
     {
         HungerBar.value = hunger;
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        getHunger();
-        UpdateScreen();
-    }
-
-    public void CloseGame()
-    {
-        Application.Quit();
-    }
-
-    public void LoadScentScene()
-    {
-        SceneManager.LoadScene("Path Testing");
     }
 }
