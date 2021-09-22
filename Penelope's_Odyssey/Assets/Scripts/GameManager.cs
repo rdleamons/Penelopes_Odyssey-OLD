db7ccecf90d9985acc1f4a7f1a5475f9951120cd
DRaven_McCoy <dravenmccoy1@gmail.com>
2021-09-22 17:20:10 -0500

    Revert "Created a pause menu, and unblurred the text on the start screen buttons."
    This reverts commit 3d9dd89765ecef7a6f4ac93cdeddde8803d31976.

--- Penelope's_Odyssey/Assets/Scripts/GameManager.cs
+++ Penelope's_Odyssey/Assets/Scripts/GameManager.cs
@@ -3,14 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
 using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    [Tooltip("Root GameObject of the menu used to toggle its activation")]
-    public GameObject MenuRoot;
-
     public float hunger = 100;
     public float sub = 2;
     public Slider HungerBar;
@@ -18,51 +14,21 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        MenuRoot.SetActive(false);
         loseText.enabled = false;
     }
 
     void Update()
     {
+        getHunger();
+        UpdateScreen();
 
-        if (MenuRoot.activeSelf)
-        {
-             getHunger();
-            UpdateScreen();
-        }
-        else
-        {
-            changeHunger();
-            UpdateScreen();
-        }
-       
-
-        // Lock cursor when clicking outside of menu
-        if (!MenuRoot.activeSelf && Input.GetMouseButtonDown(0))
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-            SetPauseMenuActivation(!MenuRoot.activeSelf);
-        }
-
-        if (hunger <= 0)
+        if(hunger <= 0)
         {
             StartCoroutine("lose");
         }
     }
 
     public float getHunger()
-    {
-        return hunger;
-    }
-
-    public float changeHunger()
     {
         hunger = hunger - sub / 180;
         return hunger;
@@ -73,31 +39,6 @@ public class GameManager : MonoBehaviour
         HungerBar.value = hunger;
     }
 
-    public void ClosePauseMenu()
-    {
-        SetPauseMenuActivation(false);
-    }
-
-    void SetPauseMenuActivation(bool active)
-    {
-        MenuRoot.SetActive(active);
-
-        if (MenuRoot.activeSelf)
-        {
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-            Time.timeScale = 0f;
-
-            EventSystem.current.SetSelectedGameObject(null);
-        }
-        else
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-            Time.timeScale = 1f;
-        }
-
-    }
     IEnumerator lose()
     {
         loseText.enabled = true;
