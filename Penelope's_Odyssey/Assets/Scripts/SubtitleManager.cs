cfe320440e54b8c6de764618a233a8975476eeec
Rebekah <rdleamons@gmail.com>
2021-09-22 15:12:44 -0500

    Added narration scripts.

---
+++ Penelope's_Odyssey/Assets/Scripts/SubtitleManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SubtitleManager : MonoBehaviour
+{
+
+    public GameObject textBox;
+    
+    void Start()
+    {
+        StartCoroutine("Sequence");
+    }
+
+    IEnumerator Sequence()
+    {
+        yield return new WaitForSeconds(1);
+        textBox.GetComponent<Text>().text = "WASD to move.";
+        yield return new WaitForSeconds(3);
+        textBox.GetComponent<Text>().text = "";
+        yield return new WaitForSeconds(1);
+        textBox.GetComponent<Text>().text = "Space to jump.";
+        yield return new WaitForSeconds(3);
+        textBox.GetComponent<Text>().text = "";
+        yield return new WaitForSeconds(1);
+        textBox.GetComponent<Text>().text = "Click to stop & sniff.";
+        yield return new WaitForSeconds(3);
+        textBox.GetComponent<Text>().text = "";
+
+    }
+
+}
