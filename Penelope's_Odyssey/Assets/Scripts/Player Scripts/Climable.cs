beb55bc1ff7b9a48570673aa3cb573a0f4aea97e
JoshuaCCrass <JCCrass3139@gmail.com>
2021-09-22 08:01:16 -0500

    Movement and Climbing
    The movement didn't change much because of problems with other scripts. And the Climbing work but it has problems with player getting stuck and going up to fast.

---
+++ Penelope's_Odyssey/Assets/Scripts/Player Scripts/Climable.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Climable : MonoBehaviour
+{
+
+    public Transform chController;
+    bool inside = false;
+    public float speedUpDown = 3.2f; //Speed for how fast going up and down 
+    public ThirdPersonMovement TPInput; //calling the ThirdPersonMovement Script
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TPInput = GetComponent<ThirdPersonMovement>();
+        inside = false;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Climable") //check if tag Climable is on object with colliders 
+        {
+            TPInput.enabled = false;
+            inside = !inside;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Climable")
+        {
+            TPInput.enabled = true;
+            inside = !inside;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(inside == true && Input.GetKey("w")) //move up object
+        {
+            chController.transform.position += Vector3.up/speedUpDown;
+        }
+
+
+      // if (inside == true && Input.GetKey("s"))
+        //{
+            //chController.transform.position += Vector3.down/speedUpDown; //Control to go down but didn't work
+       // }
+
+    }
+}
