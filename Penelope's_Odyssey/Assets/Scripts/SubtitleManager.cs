f9afe5ddfc968fb19164b093b6597fe3789fd27a
Rebekah <rdleamons@gmail.com>
2021-09-28 20:05:54 -0500

    Added text explaining hunger meter

--- Penelope's_Odyssey/Assets/Scripts/SubtitleManager.cs
+++ Penelope's_Odyssey/Assets/Scripts/SubtitleManager.cs
@@ -19,12 +19,19 @@ public class SubtitleManager : MonoBehaviour
         textBox.GetComponent<Text>().text = "WASD to move.";
         yield return new WaitForSeconds(3);
         textBox.GetComponent<Text>().text = "";
+
         yield return new WaitForSeconds(1);
         textBox.GetComponent<Text>().text = "Space to jump.";
         yield return new WaitForSeconds(3);
         textBox.GetComponent<Text>().text = "";
+
         yield return new WaitForSeconds(1);
         textBox.GetComponent<Text>().text = "Click to stop & sniff.";
+        yield return new WaitForSeconds(3);
+        textBox.GetComponent<Text>().text = "";
+
+        yield return new WaitForSeconds(1);
+        textBox.GetComponent<Text>().text = "Watch your hunger meter -- don't let Penelope starve!";
         yield return new WaitForSeconds(3);
         textBox.GetComponent<Text>().text = "";
 
